b0d9fe951a88c00b4b2a364371d991242900cb9d
Angela Mancini <angela.mancini@sage.com>
2016-06-23 15:37:34 -0400

    posts with http statuses

--- lib/s3.rb
+++ lib/s3.rb
@@ -2,7 +2,7 @@ require 'aws-sdk'
 
 class S3
   def initialize(data)
-    puts data
+    # puts data
     region = data['region'] ||= 'us-east-1'
     @source_bucket = data['source_bucket']
     @file_name = data['file_name']
@@ -12,21 +12,48 @@ class S3
     )
   end
 
-  def get_bucket
-    @s3.bucket(@source_bucket)
+  def get_bucket(bucket)
+    @s3.bucket(bucket)
   end
 
   # does the file exist in the bucket?
+  def check_file(bucket, file)
+    bucket = get_bucket(bucket)
+    response = bucket.object(file).exists?
+    if response
+      return true
+    else
+      return false
+    end
+  end
+
   def file_exists
-    bucket = get_bucket
-    bucket.object(@file_name).exists?
+    if check_file(@source_bucket, @file_name)
+      [200,{ message: "#{file} exists." }.to_json]
+    else
+      [404,{ message: "#{file} not found." }.to_json]
+    end
   end
 
   # copy specific file from one folder to another
   def copy_file
-    bucket = get_bucket
+    bucket = get_bucket(@source_bucket)
     object = bucket.object(@file_name)
-    object.copy_to(@destination)
+    destination_bucket = @destination.split('/').first
+    destination_file = @destination.gsub("#{destination_bucket}/",'')
+    dest_file_already_there = check_file(destination_bucket,destination_file)
+    if check_file(@source_bucket, @file_name)
+      if dest_file_already_there
+        [200,{ message: "#{@file_name} exists." }.to_json]
+      else
+        object.copy_to(@destination)
+        # if object copies, return a 201
+        [201,{ message: "#{@file_name} copied." }.to_json]
+        # if object does not copy, return appropriate error
+      end
+    else
+      [404,{ message: "#{@file_name} not found." }.to_json]
+    end
   end
 
 end
